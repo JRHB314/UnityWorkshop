e176909b5c6ffbf8e14ef195cef6de33035ab157
JRHB314 <gardeningblueroses@gmail.com>
2018-12-17 13:34:34 -0800

    Create CharMove.cs

---
+++ Code/CharMove.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private float holdX;
+	private float holdY;
+	private bool moving;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moving = false;
+
+		float horiz = Input.GetAxisRaw("Horizontal");
+		float vert = Input.GetAxisRaw("Vertical");
+
+		if(horiz > 0f || horiz < 0f) {
+			float movement = horiz * moveSpeed * Time.deltaTime;
+			transform.Translate(new Vector3(movement, 0f, 0f));
+			moving = true;
+			holdX = horiz;
+			holdY= 0;
+		}
+		if(vert > 0f || vert < 0f) {
+			float movement = vert * moveSpeed * Time.deltaTime;
+			transform.Translate(new Vector3(0f, movement, 0f));
+			moving = true;
+			holdY = vert;
+			holdX = 0;
+		}
+		
+		anim.SetFloat("xMvmt", horiz);
+		anim.SetFloat("yMvmt", vert); 
+		anim.SetFloat("lastX", holdX);
+		anim.SetFloat("lastY", holdY);
+		anim.SetBool("isMoving", moving);
+	}
+}
